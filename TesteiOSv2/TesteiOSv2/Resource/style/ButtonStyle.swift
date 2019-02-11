8e6c310e16afe90a1d0a649e8130b18e3beba7b9
Anderson Silva <anderson.jesus.silva@accenture.com>
2019-02-11 15:45:10 -0300

    Estilos.

---
+++ TesteiOSv2/TesteiOSv2/Resource/style/ButtonStyle.swift
@@ -0,0 +1,81 @@
+//
+//  ButtonStyle.swift
+//  TesteiOSv2
+//
+//  Created by anderson.jesus.silva on 11/02/2019.
+//  Copyright © 2019 anderson.jesus.silva. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ButtonStyle : UIButton {
+    @IBInspectable var cornerRadius : CGFloat = 0{
+        didSet{
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth : CGFloat = 0 {
+        didSet{
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor : UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+    
+}
+
+
