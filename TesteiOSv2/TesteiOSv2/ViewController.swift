45fd6d9eadff8fbdbc976d00771169cea42b9e70
Anderson Silva <anderson.jesus.silva@accenture.com>
2019-02-11 10:23:42 -0300

    Create project.

---
+++ TesteiOSv2/TesteiOSv2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteiOSv2
+//
+//  Created by anderson.jesus.silva on 11/02/2019.
+//  Copyright © 2019 anderson.jesus.silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
